9ef172804878c4e68b5f9ba3ec6bcb873d00fabe
chris <wongislandd@gmail.com>
2024-03-01 11:42:56 -0500

    add same word and extra validation for fake characters

--- formatter.py
+++ formatter.py
@@ -1,5 +1,14 @@
+import re
+
+def is_valid_word(word):
+    return bool(re.match(r'^[a-zA-Z\-]+$', word))
+
 # Completely invalidate words
-def validate(targetWord, similarWord) -> bool:
+def validate(similarWord, targetWord, knownWords) -> bool:
+    if not is_valid_word(similarWord):
+        return False
+    if similarWord in knownWords:
+        return False
     if targetWord in similarWord:
         return False
     else:
@@ -22,11 +31,15 @@ def sort(wordAndScores):
 
 def formatResults(targetWord, similarities):
     result = [[targetWord, 0]]
+    knownWords = set()
     count = 1
+    # Goes in order of similarity
     for similarity in similarities:
-        similarWord = similarity[0]
-        if validate(targetWord, similarWord):
+        similarWord = similarity[0].lower()
+        # Skip words that are the same, probably should skip plural too (?)
+        if validate(similarWord, targetWord, knownWords):
             result.append([clean(similarWord), count])
+            knownWords.add(similarWord)
             count += 1
     return sort(result)
 
@@ -41,7 +54,7 @@ testInput = [('kings', 0.7138045430183411),
              ('ruler', 0.5797567367553711),
              ('princes', 0.5646552443504333),
              ('throne', 0.5422105193138123),
-             ('royal', 0.5239794254302979)]
+             ('#/#-inch', 0.5239794254302979)]
 
 import sys
 
