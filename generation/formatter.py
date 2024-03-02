e7f0d9954bc32d046e0e818d9a5fe1cd88755a1a
chris <wongislandd@gmail.com>
2024-03-01 19:18:47 -0500

    refined formatter

--- generation/formatter.py
+++ generation/formatter.py
@@ -1,5 +1,10 @@
 import re
 
+import nltk
+from nltk.corpus import wordnet as wn
+
+nltk.download('wordnet')
+
 def is_valid_word(word):
     return bool(re.match(r'^[a-zA-Z\-]+$', word))
 
@@ -14,12 +19,24 @@ def validate(similarWord, targetWord, knownWords) -> bool:
     else:
         return True
 
+# Use synsets to get more words from each match (this kinda inflates our numbers)
+def expandWord(originalWord):
+    results = set()
+    results.add(originalWord)
+    synsets = wn.synsets(originalWord, pos="n")
+    if len(synsets) == 0:
+        return results
+    for synset in synsets:
+        results.add(synset.lemmas()[0].name())
+    return results
 
 # Enforce conventions (e.g no spaces, underscores, etc.)
 def clean(word) -> str:
     # For now, if the word has multiple words, take the first one. "crown_prince" -> "prince"
-    return word.split("_")[0]
+    return nounify(word).split("_")[0]
 
+def nounify(word) -> str:
+    return word
 
 # Sort the results alphabetically to optimize searching, each word should have the proper index
 def sort(wordAndScores):
@@ -38,9 +55,12 @@ def formatResults(targetWord, similarities):
         similarWord = similarity[0].lower()
         # Skip words that are the same, probably should skip plural too (?)
         if validate(similarWord, targetWord, knownWords):
-            result.append([clean(similarWord), count])
-            knownWords.add(similarWord)
-            count += 1
+            for expandedWord in expandWord(similarWord):
+                cleanedWord = clean(expandedWord)
+                if validate(expandedWord, targetWord, knownWords):
+                    result.append([cleanedWord, count])
+                    knownWords.add(expandedWord)
+                    count += 1
     return sort(result)
 
 
@@ -52,11 +72,10 @@ testInput = [('kings', 0.7138045430183411),
              ('prince', 0.6159993410110474),
              ('sultan', 0.5864824056625366),
              ('ruler', 0.5797567367553711),
-             ('princes', 0.5646552443504333),
+             ('running', 0.5646552443504333),
              ('throne', 0.5422105193138123),
              ('#/#-inch', 0.5239794254302979)]
 
-import sys
 
 if __name__ == '__main__':
     print(formatResults(testWord, testInput))
