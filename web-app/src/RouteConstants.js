81822ebefaea15860319b82e74aac8779a82dcc8
Christopher Wong <cwong51799@gmail.com>
2024-03-04 16:01:35 -0500

    Fixing routes by removing port

--- web-app/src/RouteConstants.js
+++ web-app/src/RouteConstants.js
@@ -3,11 +3,10 @@ const USING_LOCAL_SERVER = process.env.NODE_ENV === "development"
 const USING_HTTP = process.env.NODE_ENV === "development"
 
 const REMOTE_BASE_URL = "wordlink.azurewebsites.net"
-const LOCAL_BASE_URL = "localhost"
+const LOCAL_BASE_URL = "localhost:8080"
 const SCHEMA = USING_HTTP ? "http://" : "https://"
-const PORT = USING_HTTP ? ":8080" : ":8444"
 const USED_URL = USING_LOCAL_SERVER ? LOCAL_BASE_URL : REMOTE_BASE_URL
-const BASE_URL = SCHEMA + USED_URL + PORT
+const BASE_URL = SCHEMA + USED_URL
 const GUESS_API_ROUTE = BASE_URL + "/guess"
 const GET_GAME_IDS_ROUTE = BASE_URL + "/games" 
 const GET_GAME_DETAILS_ROUTE = BASE_URL + "/games/details"
