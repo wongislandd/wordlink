ef0ca7bae3991c1b8343431d69dcb0c133f4f736
Christopher Wong <cwong51799@gmail.com>
2024-03-04 10:15:02 -0500

    using http

--- web-app/src/RouteConstants.js
+++ web-app/src/RouteConstants.js
@@ -3,9 +3,11 @@ const USING_LOCAL_SERVER = process.env.NODE_ENV === "development"
 
 const REMOTE_BASE_URL = "20.115.73.149"
 const LOCAL_BASE_URL = "localhost"
+//const SCHEMA = USING_LOCAL_SERVER ? "http://" : "https://"
 const SCHEMA = USING_LOCAL_SERVER ? "http://" : "http://"
 const USED_URL = USING_LOCAL_SERVER ? LOCAL_BASE_URL : REMOTE_BASE_URL
-const PORT = USING_LOCAL_SERVER ? ":8080" : ":8444"
+//const PORT = USING_LOCAL_SERVER ? ":8080" : ":8444"
+const PORT = USING_LOCAL_SERVER ? ":8080" : ":8080"
 const BASE_URL = SCHEMA + USED_URL + PORT
 const GUESS_API_ROUTE = BASE_URL + "/guess"
 const GET_GAME_IDS_ROUTE = BASE_URL + "/games" 
